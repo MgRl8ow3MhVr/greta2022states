101739b4cb5996b7f3b7ece942d07d8a273ab340
Pierre <pierre@netdevices.fr>
2022-05-16 14:42:15 +0200

    add compteurs

--- src/components/Compteurs/Compteurs.js
+++ src/components/Compteurs/Compteurs.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Compteurs.css";
 
 const Compteurs = () => {
-  return <div className="compteursContainer">HELLO</div>;
+  const [compteur1, setCompteur1] = useState(0);
+
+  return (
+    <div className="compteursContainer">
+      {compteur1 <= 10 ? (
+        <button
+          onClick={() => {
+            setCompteur1(compteur1 + 1);
+          }}
+        >
+          +
+        </button>
+      ) : null}
+
+      <div>{compteur1}</div>
+      {compteur1 >= 0 ? (
+        <button
+          onClick={() => {
+            setCompteur1(compteur1 - 1);
+          }}
+        >
+          -
+        </button>
+      ) : null}
+    </div>
+  );
 };
 export default Compteurs;
